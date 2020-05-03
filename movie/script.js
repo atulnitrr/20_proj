1b651ec676eae01c0f62e5895b953b5b8e93669d
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-03 09:21:11 +0530

    Updat price

--- movie/script.js
+++ movie/script.js
@@ -1,1 +1,32 @@
 console.log("movie ticket booking");
+const container = document.querySelector(".container");
+const availableSeats = document.querySelectorAll(".row .seat:not(.occupied)");
+
+const count = document.querySelector(".count");
+const total = document.querySelector(".total");
+const movieSelect = document.querySelector("#movie");
+let ticketPrice = +movieSelect.value;
+console.log(movieSelect);
+
+movieSelect.addEventListener("change", (event) => {
+  console.log(event.target.value);
+  ticketPrice = +event.target.value;
+  updatePrice();
+});
+
+const updatePrice = () => {
+  const selectedSeat = document.querySelectorAll(".row .seat.selected ");
+  count.textContent = selectedSeat.length;
+  total.textContent = ticketPrice * selectedSeat.length;
+};
+
+container.addEventListener("click", (event) => {
+  if (
+    event.target.classList.contains("seat") &&
+    !event.target.classList.contains("occupied")
+  ) {
+    event.target.classList.toggle("selected");
+    console.log(event.target);
+    updatePrice();
+  }
+});
